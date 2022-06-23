a0ec00a37bfcf221f5aa0493ceedbf9609f388a2
Rodrigo Hervas <38118239+rodrigohervas@users.noreply.github.com>
2022-06-23 18:06:49 +0200

    modified knexContext to remove ssl prop in knex connection

--- src/knexContext.js
+++ src/knexContext.js
@@ -6,8 +6,7 @@ const config = require('./config');
  */
 const db = knex({
     client: 'pg', 
-    connection: (config.NODE_ENV === 'test') ? config.TEST_DATABASE_URL : config.DATABASE_URL, 
-    ssl: false
+    connection: (config.NODE_ENV === 'test') ? config.TEST_DATABASE_URL : config.DATABASE_URL
 });
 
 module.exports = db;
